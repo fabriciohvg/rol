15549a444872d47a07686c865eac43a169f8dc04
Fabrício G. <fabriciohenrique.eng@gmail.com>
2026-03-05 12:01:10 -0300

    theme-provider.tsx movido para ./components/providers; tanstack devtools instalado para forms e query.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers/query-provider";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
