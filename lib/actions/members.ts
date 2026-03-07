085455a3a9ff4928ffe1245aeb41f46535ff2350
Fabrício G. <fabriciohenrique.eng@gmail.com>
2026-03-07 18:34:24 -0300

    feature: testing full text search supabase

--- lib/actions/members.ts
+++ lib/actions/members.ts
@@ -13,6 +13,15 @@ export async function getMembros(): Promise<Membro[]> {
     .order("nome", { ascending: true })
     .limit(20); // Limit to 100 members for performance
 
+  // Full Text search for members based on the search query using unaccent and to_tsvector
+  /*   const searchQuery = "fabricio"; // Replace with actual search query
+  const { data, error } = await supabase.rpc(
+    "search_membros_by_name_unaccent",
+    { search_term: searchQuery },
+  ); */
+
+  console.log("Data fetched from Supabase:", data);
+
   if (error) {
     console.error("Erro fetching membros:", error);
     throw new Error("Erro fetching membros");
